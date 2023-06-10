dc7f0d463c71ffad2fbda693126ec7e3bc1520bd
JavierP08 <A01662438@tec.mx>
2023-06-10 10:03:22 -0600

    Test

--- cpu/src/test_cpu.py
+++ cpu/src/test_cpu.py
@@ -1,20 +1,86 @@
 import unittest
+import numpy as np
+from your_module import (
+    turn90,
+    turn180,
+    turn270,
+    movement,
+    read_file
+)
 
-class TestStringMethods(unittest.TestCase):
+class YourTestCase(unittest.TestCase):
+    def test_turn90(self):
+        dir = "UP"
+        turn90()
+        self.assertEqual(dir, "RIGHT")
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        dir = "RIGHT"
+        turn90()
+        self.assertEqual(dir, "DOWN")
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        dir = "DOWN"
+        turn90()
+        self.assertEqual(dir, "LEFT")
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        dir = "LEFT"
+        turn90()
+        self.assertEqual(dir, "UP")
+
+    def test_turn180(self):
+        dir = "UP"
+        turn180()
+        self.assertEqual(dir, "DOWN")
+
+        dir = "RIGHT"
+        turn180()
+        self.assertEqual(dir, "LEFT")
+
+        dir = "DOWN"
+        turn180()
+        self.assertEqual(dir, "UP")
+
+        dir = "LEFT"
+        turn180()
+        self.assertEqual(dir, "RIGHT")
+
+    def test_turn270(self):
+        dir = "UP"
+        turn270()
+        self.assertEqual(dir, "LEFT")
+
+        dir = "RIGHT"
+        turn270()
+        self.assertEqual(dir, "UP")
+
+        dir = "DOWN"
+        turn270()
+        self.assertEqual(dir, "RIGHT")
+
+        dir = "LEFT"
+        turn270()
+        self.assertEqual(dir, "DOWN")
+
+    def test_movement(self):
+        map_field = np.zeros(shape=(10, 10))
+        x = 0
+        y = 0
+        dir = "RIGHT"
+
+        movement(3)
+        expected_map_field = np.zeros(shape=(10, 10))
+        expected_map_field[0][0] = 1
+        expected_map_field[0][1] = 1
+        expected_map_field[0][2] = 1
+        expected_map_field[0][3] = 2
+        self.assertTrue(np.array_equal(map_field, expected_map_field))
+        self.assertEqual(x, 3)
+        self.assertEqual(y, 0)
+        self.assertEqual(dir, "RIGHT")
+
+    def test_read_file(self):
+        inst_list = read_file()
+        self.assertIsInstance(inst_list, list)
+        self.assertTrue(all(isinstance(row, list) for row in inst_list))
 
 if __name__ == '__main__':
     unittest.main()
